e1c9b3dea677a496aedfcb84e79a499b4dd502ab
nyberry <nckbrry@gmail.com>
2024-06-18 10:55:24 +0300

    added buttons to welcome screen

--- dinner/views.py
+++ dinner/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+game_started=False
+
 def welcome(request):
-    return render(request, 'dinner/welcome.html', {})
+    global game_started
+    action = request.GET.get('action')
+    if action == 'start':
+        # Handle the logic for starting a new game
+        game_started= True
+        return HttpResponse("Starting a new game...")
+    elif action == "join":
+        # Handle the logic for starting a new game
+        return HttpResponse("Joining the game...")
+    else:
+        # Default behavior for the index route
+        return render(request, 'dinner/welcome.html', {"game_started":game_started})
